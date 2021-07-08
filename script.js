fbaeb7b1f5dde3d6d7e0795400d3a0e30aef68ba
Victor Shan <victorshanvs@gmail.com>
2021-07-07 22:59:51 -0700

    popup attempts

--- script.js
+++ script.js
@@ -19,56 +19,88 @@
     }
 
     function listenToDB(map) {
+
+        /**
+         * A customized popup on the map.
+         */
+        class Popup extends google.maps.OverlayView {
+            position;
+            containerDiv;
+            constructor(position, content) {
+                super();
+                this.position = position;
+                content.classList.add("popup-bubble");
+                // This zero-height div is positioned at the bottom of the bubble.
+                const bubbleAnchor = document.createElement("div");
+                bubbleAnchor.classList.add("popup-bubble-anchor");
+                bubbleAnchor.appendChild(content);
+                // This zero-height div is positioned at the bottom of the tip.
+                this.containerDiv = document.createElement("div");
+                this.containerDiv.classList.add("popup-container");
+                this.containerDiv.appendChild(bubbleAnchor);
+                // Optionally stop clicks, etc., from bubbling up to the map.
+                Popup.preventMapHitsAndGesturesFrom(this.containerDiv);
+            }
+
+            /** Called when the popup is added to the map. */
+            onAdd() {
+                this.getPanes().floatPane.appendChild(this.containerDiv);
+            }
+
+            /** Called when the popup is removed from the map. */
+            onRemove() {
+                if (this.containerDiv.parentElement) {
+                    this.containerDiv.parentElement.removeChild(this.containerDiv);
+                }
+            }
+
+            /** Called each frame when the popup needs to draw itself. */
+            draw() {
+                const divPosition = this.getProjection().fromLatLngToDivPixel(
+                    this.position
+                );
+                // Hide the popup when it is far out of view.
+                const display =
+                    Math.abs(divPosition.x) < 4000 && Math.abs(divPosition.y) < 4000
+                    ? "block"
+                    : "none";
+
+                if (display === "block") {
+                    this.containerDiv.style.left = divPosition.x + "px";
+                    this.containerDiv.style.top = divPosition.y + "px";
+                }
+
+                if (this.containerDiv.style.display !== display) {
+                    this.containerDiv.style.display = display;
+                }
+            }
+        }
         firebase.database().ref("locations").on('value', snapshot => {
             const data = snapshot.val();
             console.log(data);
             for (const location in locations) {
                 locations[location].marker.setMap(null);
-                delete markers[location]
+                delete markers[location];
             }
+            const markerContainer = document.getElementById("markers");
+            markerContainer.innerHTML = "";
+
             for (const location in data) {
                 locations[location] = data[location];
-                locations[location].marker = genMarker(location, data[location], map);
+                const container = gen("div");
+                container.appendChild(genCardTitle(location));
+                container.appendChild(
+                    genCardTime(
+                        locations[location].queue,
+                        locations[location].workers,
+                        locations[location].workRate
+                    )
+                );
+                markerContainer.appendChild(container);
+                data.marker = new Popup(new google.maps.LatLng(locations[location].lat, locations[location].long), container);
+                data.marker.setMap(map);
             }
         });
-    }
-
-    function genMarker(name, data, map) {
-        const container = gen("div");
-        container.appendChild(genCardTitle(name));
-        container.appendChild(genCardTime(data.queue, data.workers, data.workRate));
-
-        const infoWindow = new google.maps.InfoWindow({
-            content: convertDOMToString(container),
-        });
-        console.log(parseFloat(data.lat), parseFloat(data.long))
-        const marker = new google.maps.Marker({
-            position: { lat: parseFloat(data.lat), lng: parseFloat(data.long) },
-            map,
-            title: name,
-        });
-        console.log(marker);
-
-        let isOpen = true;
-        infoWindow.open({
-            anchor: marker,
-            map,
-            shouldFocus: false,
-        });
-        marker.addListener("click", () => {
-            if (isOpen) {
-                infoWindow.close();
-            } else {
-                infoWindow.open({
-                    anchor: marker,
-                    map,
-                    shouldFocus: false,
-                });
-            }
-            isOpen = !isOpen;
-        });
-
-        return marker;
     }
 
     function convertDOMToString(element) {
